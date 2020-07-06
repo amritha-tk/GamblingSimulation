5d3b4f65a082e0d80d647833076f45fc91cf633f
amritha-tk <amrithatk95@gmail.com>
2020-07-06 18:44:15 +0530

    [amritha]Added UC3_Condition to resign

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -3,13 +3,22 @@
 
 totalAmount=100
 betAmount=1
+#read -p "Enter the times" n
+for(( i=0; $totalAmount<150 && $totalAmount>0; i++ ))
+do
 betResult=$(( (RANDOM%2) +1 ))
-
 if [ $betResult -eq 1 ]
   then
       ((totalAmount++));
-       echo "Gambler won" $totalAmount
   else
       ((totalAmount--));
-       echo "Gambler loss" $totalAmount
 fi
+done
+if [ $totalAmount -ge 150 ]
+then
+  echo "Gambler stopped and won !!" $totalAmount
+elif [ $totalAmount -le 50 ]
+  then
+echo "Gambler stopped and lost !!" $totalAmount
+fi
+
