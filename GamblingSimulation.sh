409aecc853e10fe2cc09ff8fd4327b0324a1be9a
amritha-tk <amrithatk95@gmail.com>
2020-07-07 19:48:21 +0530

    [amritha]Added UC6 with proper indentation

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,31 +1,64 @@
-
-#!/bin/bash -x
+#!/bin/bash
 
 DAY=0
-MAXDAY=30
-MAX_STAKE=150
-MIN_STAKE=50
-echo  "starting the  game"
-while [[ $DAY -lt MAXDAY ]]
- do
-  TOTAL_AMOUNT=100
-  for(( i=0; (( $TOTAL_AMOUNT < MAX_STAKE )) && (( $TOTAL_AMOUNT > MIN_STAKE )); i++ ))
-    do
-      betResult=$(( (RANDOM%2) +1 ))
-       if [ $betResult -eq 1 ]
-         then
-            ((TOTAL_AMOUNT++));
-        else
-            ((TOTAL_AMOUNT--));
-       fi
-  done
-   ((DAY++))
-       if [ $TOTAL_AMOUNT==MAX_STAKE ]
-        then
-          result="won"
-       elif [ $TOTAL_AMOUNT==MIN_STAKE ]
-         then
-           result="lost"
-       fi
-         echo "Gambler stopped and " $result " for day"$DAY" amount  !!" $TOTAL_AMOUNT
-done
+MAXDAY=5
+MAX_STAKE=1.5
+MIN_STAKE=0.5
+dayStart=0
+dayIncrement=100
+max_Win=0
+max_Loss=0
+
+echo  "Starting the  game"
+
+function MonthCalculation()
+
+  {
+	while [[ $DAY -lt MAXDAY ]]
+	 do
+	  dayStart=$(( $dayIncrement + $dayStart ))
+	  percentMaxStake=$(($dayStart*3/2 ))
+	  percentMinStake=$(($dayStart/2 ))
+
+		for(( i=0; (( $dayStart <  $percentMaxStake )) && (( $dayStart > $percentMinStake )); i++ ))
+		 do
+		    betResult=$(( (RANDOM%2) +1 ))
+		    if [ $betResult -eq 1 ]
+			 then
+				((dayStart++));
+			else
+				((dayStart--));
+		   fi
+	     done
+	  ((DAY++))
+
+		   if [ $dayStart == $percentMaxStake ]
+			then
+			  result="won"
+			  dayProfit=$(($dayStart))
+
+			  if [ $dayProfit > $max_Win ]
+			    then
+				 max_Win=$(($dayProfit))
+				 max_Win_Day=$DAY
+			  fi
+		   elif [ $dayStart == $percentMinStake ]
+			 then
+			   result="lost"
+			   dayLoss=$dayStart
+
+			   if [ $dayLoss > $max_Loss ]
+				 then
+				  max_Loss=$dayLoss
+				  max_Loss_Day=$DAY
+			   fi
+		   fi
+			echo "Gambler stopped and " $result " for day"$DAY" amount  !!" $dayStart
+	done
+
+	echo " For the Month -- Maximum Won amount is " $max_Win "on Day:" $max_Win_Day "and Maximum Lost amount " $max_Loss " on Day:" $max_Loss_Day
+
+  }
+
+MonthCalculation
+
