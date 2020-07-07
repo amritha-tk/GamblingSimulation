aeae9c43b79867f6068df427a35614c883ba350c
amritha-tk <amrithatk95@gmail.com>
2020-07-07 21:06:50 +0530

    [amritha]Added condition for UC7

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 DAY=0
-MAXDAY=30
-MAX_STAKE=1.5
-MIN_STAKE=0.5
+MAXDAY=5
 dayStart=0
 dayIncrement=100
 max_Win=0
 max_Loss=0
-
+winCount=0
+lossCount=0
 echo  "Starting the  game"
 
 function MonthCalculation()
@@ -16,9 +15,9 @@ function MonthCalculation()
   {
 	while [[ $DAY -lt MAXDAY ]]
 	 do
-	  dayStart=$(( $dayIncrement + $dayStart ))
-	  percentMaxStake=$(($dayStart*3/2 ))
-	  percentMinStake=$(($dayStart/2 ))
+	   dayStart=$(( $dayIncrement + $dayStart ))
+	   percentMaxStake=$(($dayStart*3/2 ))
+	   percentMinStake=$(($dayStart/2 ))
 
 		for(( i=0; (( $dayStart <  $percentMaxStake )) && (( $dayStart > $percentMinStake )); i++ ))
 		 do
@@ -36,7 +35,7 @@ function MonthCalculation()
 			then
 			  result="won"
 			  dayProfit=$(($dayStart))
-
+                           ((winCount++))
 			  if [ $dayProfit -gt $max_Win ]
 			    then
 				 max_Win=$(($dayProfit))
@@ -46,17 +45,26 @@ function MonthCalculation()
 			 then
 			   result="lost"
 			   dayLoss=$dayStart
-
+                           ((lossCount++))
 			   if [ $dayLoss -gt $max_Loss ]
 				 then
 				  max_Loss=$(($dayLoss))
 				  max_Loss_Day=$(($DAY))
 			   fi
 		   fi
+
+
 			echo "Gambler stopped and " $result " for day"$DAY" amount  !!" $dayStart
 	done
 
 	echo " For the Month -- Maximum Won amount is " $max_Win "on Day:" $max_Win_Day "and Maximum Lost amount " $max_Loss " on Day:" $max_Loss_Day
+
+         if [ $winCount -gt $lossCount ]
+              then
+                echo "Gambler  continues next month!!"
+          else
+                echo "Gambler Stopped and will not play for next month !!"
+         fi
 
   }
 
